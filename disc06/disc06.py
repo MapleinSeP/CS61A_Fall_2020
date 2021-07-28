1dc8ae214fe9a3524d6006aa81b0890e4640302e
Maple <82840711+MapleinSeP@users.noreply.github.com>
2021-07-28 12:08:41 +0800

    initial commit

---
+++ disc06/disc06.py
@@ -0,0 +1,16 @@
+# Question 1.1
+def memory(n):
+    """
+    >>> f = memory(10)
+    >>> f(lambda x: x * 2)
+    20
+    >>> f(lambda x: x - 7)
+    13
+    >>> f(lambda x: x > 5)
+    True
+    """
+    def f(g):
+        nonlocal n
+        n = g(n)
+        return n
+    return f
